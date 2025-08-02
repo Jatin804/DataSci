845e16f24c68332ccc310185c2eb32c3d512bed9
Jatin ; ) <146949850+Jatin804@users.noreply.github.com>
2025-08-02 17:23:30 +0530

    Update data_ingestion.py

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -14,6 +14,9 @@ from dataclasses import dataclass       # creating class variables
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransFConfig
 
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
+
 
 
 
@@ -59,3 +62,8 @@ if __name__ == "__main__":
 
     data_transformation = DataTransformation()
     train_arr, test_arr,_ = data_transformation.initiate_data_transform(train_data, test_data)
+
+    model_trainer = ModelTrainer()
+    print(model_trainer.initiate_model_trainer(train_arr, test_arr))
+
+
